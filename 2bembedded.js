8914fef5d819ad07ebb45d3904e6fc498f4c39b2
chrfoyer <chrfoyer@gmail.com>
2023-03-17 14:57:21 +0100

    Using working code for inspiration
    Used a web tips tutorial for making tic tac toe in vanilla js. I hope this can help overcome this code block I have been experiencing. I will place comments throughout to demonstrate my understanding of the functionality and then use it to make it in my own words

--- 2bembedded.js
+++ 2bembedded.js
@@ -1,54 +1,69 @@
-class Board {
-  constructor() {
-    this.matrix = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""],
-    ];
-  }
+const game = {
+  xTurn: true,
+  xState: [],
+  oState: [],
+  winningStates: [
+      // Rows
+      ['0', '1', '2'],
+      ['3', '4', '5'],
+      ['6', '7', '8'],
 
-  place(row, column, player) {
-    if ((this.matrix[row][column] = "")) {
-      throw new Error("That space is already taken");
-    } else if (player != "x" && player != "o") {
-      throw new Error("You can only place an x or an o");
-    } else {
-      this.matrix[row][column] = player;
-    }
-  }
+      // Columns
+      ['0', '3', '6'],
+      ['1', '4', '7'],
+      ['2', '5', '8'],
 
-  isWinner() {
-    if (this.matrix [0][0] == 'x' && this.matrix [0][1] == 'x' && this.matrix [0][2] == 'x')
-    {
-        return true;
-    }
-    if (this.matrix [1][0] == 'x' && this.matrix [1][1] == 'x' && this.matrix [1][2] == 'x')
-    {
-        return true;
-    }
-    if (this.matrix [2][0] == 'x' && this.matrix [2][1] == 'x' && this.matrix [2][2] == 'x')
-    {
-        return true;
-    }
-    if (this.matrix [0][0] == 'x' && this.matrix [1][0] == 'x' && this.matrix [2][0] == 'x')
-    {
-        return true;
-    }
-    if (this.matrix [0][1] == 'x' && this.matrix [1][1] == 'x' && this.matrix [2][1] == 'x')
-    {
-        return true;
-    }
-    if (this.matrix [0][2] == 'x' && this.matrix [1][2] == 'x' && this.matrix [2][2] == 'x')
-    {
-        return true;
-    }
-    if (this.matrix [0][0] == 'x' && this.matrix [1][1] == 'x' && this.matrix [2][2] == 'x')
-    {
-        return true;
-    }
-    if (this.matrix [0][2] == 'x' && this.matrix [1][1] == 'x' && this.matrix [2][0] == 'x')
-    {
-        return true;
-    }
-  }
+      // Diagonal
+      ['0', '4', '8'],
+      ['2', '4', '6']
+  ]
 }
+
+document.addEventListener('click', event => {
+  const target = event.target
+  const isCell = target.classList.contains('grid-cell')
+  const isDisabled = target.classList.contains('disabled')
+
+  if (isCell && !isDisabled) {
+      const cellValue = target.dataset.value
+
+      game.xTurn === true
+          ? game.xState.push(cellValue)
+          : game.oState.push(cellValue)
+
+      target.classList.add('disabled')
+      target.classList.add(game.xTurn ? 'x' : 'o')
+
+      game.xTurn = !game.xTurn
+      
+      // If all cells are disabled, then its draw
+      if (!document.querySelectorAll('.grid-cell:not(.disabled)').length) {
+          document.querySelector('.game-over').classList.add('visible')
+          document.querySelector('.game-over-text').textContent = 'Draw!'
+      }
+
+      game.winningStates.forEach(winningState => {
+          const xWins = winningState.every(state => game.xState.includes(state))
+          const oWins = winningState.every(state => game.oState.includes(state))
+
+          if (xWins || oWins) {
+              document.querySelectorAll('.grid-cell').forEach(cell => cell.classList.add('disabled'))
+              document.querySelector('.game-over').classList.add('visible')
+              document.querySelector('.game-over-text').textContent = xWins
+                  ? 'X wins!'
+                  : 'O wins!'
+          }
+      })
+  }
+})
+
+document.querySelector('.restart').addEventListener('click', () => {
+  document.querySelector('.game-over').classList.remove('visible')
+  document.querySelectorAll('.grid-cell').forEach(cell => {
+      cell.classList.remove('disabled', 'x', 'o')
+  })
+
+  game.xTurn = true
+  game.xState = []
+  game.oState = []
+})
